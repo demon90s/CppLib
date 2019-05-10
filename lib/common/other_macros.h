82523f8d703203649a937384de8da61c0ee94c7c
diwen <demon90s@163.com>
2019-05-11 00:24:22 +0800

    commit

--- lib/common/other_macros.h
+++ lib/common/other_macros.h
@@ -2,3 +2,6 @@
 
 // 忽略掉 noused warning
 #define IGNORE_NOUSED_WARNING(identifier) ((void)identifier)
+
+// 对齐 example: DOALIGH(10000, 1024) -> 10240
+#define DOALIGN(x,b)   (((x) + (b) - 1) & ~((b) - 1))
